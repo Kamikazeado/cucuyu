ae110871be0928f22d9e01a2e42b0dfb9d73d2d5
Kamikazeado <109634634+Kamikazeado@users.noreply.github.com>
2024-01-09 09:51:51 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,1 +1,101 @@
 
+const keep_alive = require ('./keep_alive.js') // Intenta mantener la conexiÃ³n cada 60 // Import necessary modules from the "highrise.sdk" package
+
+
+const { Highrise, GatewayIntentBits, WebApi } = require("highrise.sdk");
+
+// Import settings for the bot
+const { authentication } = require("./config/config.js");
+const { handleReady, handleChat, handleWhisper, handleDirectMessages, handlePlayerJoin, handlePlayerLeave, handlePlayerEmotes, handlePlayerReactions, handlePlayerTips, handlePlayerMovements, handleVoiceChat, handleErrors } = require("./handlers/events.js");
+const { sendPeriodicMessage } = require("./utils/utils.js");
+
+
+// Create a new Highrise bot instance
+const bot = new Highrise({
+  intents: [
+    GatewayIntentBits.Ready,
+    GatewayIntentBits.Joins,
+    GatewayIntentBits.Leaves,
+    GatewayIntentBits.Messages,
+    GatewayIntentBits.Tips,
+    GatewayIntentBits.VoiceChat,
+    GatewayIntentBits.Reactions,
+    GatewayIntentBits.Emotes,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.Movements,
+    GatewayIntentBits.Error
+  ],
+  cache: true
+});
+
+// Event handler for when the bot is ready
+
+
+bot.on('ready', (session) => {
+  handleReady(bot, session);
+});
+
+
+// Event handler for when the player send a message
+bot.on('chatCreate', (user, message) => {
+  handleChat(bot, user, message);
+});
+
+// Event handler for when the player send a whisper
+bot.on('whisperCreate', (user, message) => {
+  handleWhisper(bot, user, message);
+});
+
+// Event handler for when the player send a direct message
+bot.on('messageCreate', (user, data) => {
+  handleDirectMessages(bot, user, data);
+});
+
+
+// Event handler for when a player joins the room
+bot.on('playerJoin', (user, react) => {
+  handlePlayerJoin(bot, user, react);
+});
+
+// Event handler for when a player leaves the room
+bot.on('playerLeave', (user) => {
+  handlePlayerLeave(bot, user);
+});
+
+// Event handler when someone perform emote
+bot.on('playerEmote', (sender, receiver, emote) => {
+  handlePlayerEmotes(bot, sender, receiver, emote);
+});
+
+// Event handler when someone perform a reaction
+bot.on('playerReact', (sender, receiver, reaction) => {
+  handlePlayerReactions(bot, sender, receiver, reaction);
+});
+
+// Event handler when someone send or receive a tip
+bot.on('playerTip', (sender, receiver, item) => {
+  handlePlayerTips(bot, sender, receiver, item);
+});
+
+// Event handler for the player's movements
+bot.on('playerMove', (user, position) => {
+  handlePlayerMovements(bot, user, position);
+});
+
+// Event handler for the voice chat
+bot.on('voiceCreate', (users, seconds_left) => {
+  handleVoiceChat(bot, users, seconds_left);
+});
+
+// Event handler for API request errors
+bot.on('error', (error) => {
+  handleErrors(error);
+});
+
+// Util handlers
+sendPeriodicMessage(bot);
+
+
+
+// Log in the bot with the provided token and room ID
+bot.login(authentication.token, authentication.room);
